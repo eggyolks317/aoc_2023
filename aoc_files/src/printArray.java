384a8cb74abfc031194feffaffccd64fb05cd795
eggyolks317 <yokshi25@kas.kh.edu.tw>
2023-12-04 18:31:31 +0800

    changed the printArray format to [ , , , ....]

--- aoc_files/src/printArray.java
+++ aoc_files/src/printArray.java
@@ -4,14 +4,14 @@ public class printArray {
     public static void string (String [] STRING) {
         System.out.print("[ ");
         for (String x:STRING) {
-            System.out.print(x+ " ; ");
+            System.out.print(x+ " , ");
         }
         System.out.println("]");
     }
     public static void integer (int [] INT) {
         System.out.print("[ ");
         for (int x:INT) {
-            System.out.print(x+ " ; ");
+            System.out.print(x+ " , ");
         }
         System.out.println("]");
     }
